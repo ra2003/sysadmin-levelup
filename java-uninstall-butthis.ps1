d7579f0e7239516564fed707c103eceee3968a12
uniquegeek <uniquegeek@gmail.com>
2020-07-22 15:15:37 -0500

    Create java-uninstall-butthis.ps1
    Uninstalls all Java JRE x86 except for a specific GUID listed in the $curr_x86. Does not uninstall Java if there is only one Java version on system.

---
+++ java-uninstall-butthis.ps1
@@ -0,0 +1,27 @@
+#uniquegeek@gmail.com
+#this script uninstalls all but a specific x86 java
+#if no java, does nothing
+#only tested with Java JRE 8u series
+
+#CHANGE THIS VARIABLE TO GUID YOU DON'T WANT UNINSTALLED
+#notice it has 80261 near end, which is 8u261
+$curr_x86= "{26A24AE4-039D-4CA4-87B4-2F32180261F0}"
+
+#for use with multi archiectures in the future:
+#$javaVer = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -match "Java 8 Update" } | Select-Object -Property DisplayName, UninstallString
+
+#get all java uninstall strings
+$javaVer = Get-ChildItem -Path HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -match "Java 8 Update" } | Select-Object -Property DisplayName, UninstallString
+
+#one java gives a System.Object type, don't uninstall
+#two or more Java verions gives System.Array type
+$multi = $javaVer -is [array]
+if ($multi) {
+    ForEach ($ver in $javaVer) {
+    	If (($ver.UninstallString) -and ($ver.UninstallString -contains $curr_x86)) {
+		    $uninst = $ver.UninstallString
+		    Start-Process cmd -ArgumentList "/c $uninst /quiet /norestart" -Verb RunAs -Wait
+		}
+    }
+ }
+ 
